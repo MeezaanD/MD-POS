ca24220ebe3ab438df4262195995b7f3fe412902
Meezaan Davids <meezaandavids365@gamil.com>
2022-12-07 11:14:18 +0200

    update

---
+++ js/checkout.js
@@ -0,0 +1,112 @@
+let products = JSON.parse(localStorage.getItem('products ')) ?
+JSON.parse(localStorage.getItem('products ')) : [
+    {
+        id: 1,
+        productName: "iPhone 14",
+        price: "R30000",
+        year: 2022,
+        image: "https://i.postimg.cc/y8tHBQ7G/iphone-14-pro-max-deep-purple-6-1.webp"
+    },
+    {
+        id: 2,
+        productName: "iPhone 13",
+        price: "R20000",
+        year: 2021,
+        image: "https://i.postimg.cc/JhZ98R6y/iphone-13-pro-green-pdp-image-position-1a-wwen-jpg-8.webp"
+    },
+    {
+        id: 3,
+        productName: "Iphone 12",
+        price: "R15000",
+        year: 2019,
+        image:"https://i.postimg.cc/k4wYMGx5/refurb-iphone-12-pro-graphite-2020.jpg"
+    },
+    {
+        id: 4,
+        productName: "Macbook Pro 16",
+        price: "R50000",
+        year: 2021,
+        image: "https://i.postimg.cc/DyKmvvPq/macbook-pro-space-grey-1-2.webp"
+    },
+    {
+        id: 5,
+        productName: "Macbook Air",
+        price: "R25000",
+        year: 2019,
+        image: "https://i.postimg.cc/hGn0VRnx/macbook-air-midnight-select-20220606.jpg"
+    },
+    {
+        id: 6,
+        productName: "Macbook Pro 13",
+        price: "R20000",
+        year: 2016,
+        image: "https://i.postimg.cc/9QRkLhsn/10282546-3-ecommerce-68f3.png"
+    },
+    {
+        id: 7,
+        productName: "Apple Smartwatch series SE",
+        price: "R8000",
+        year: 2016,
+        image: "https://i.postimg.cc/qvcGXPTB/Apple-Watch-SE-44mm-Space-Gray-Aluminum-Black-Sport-Band-MYDT2-LL-A-Refurbished.jpg" , 
+    },
+    {
+        id: 8,
+        productName: "Apple Smartwatch series 7",
+        price: "R9000",
+        year: 2019,
+        image: "https://i.postimg.cc/26dwTgv7/Apple-Watch-Series-7-GPS-41mm.jpg"
+    },
+    {
+        id: 9,
+        productName: "Apple Smartwatch series 8 ",
+        price: "R12000",
+        year: 2021,
+        image: "https://i.postimg.cc/G247QXKM/Apple-Watch-Series-8-GPS-Cellular-45mm-Graphite-Stainless-Steel-Case-with-Midnight-Sport-Band-S.jpg"
+    },
+    {
+        id: 10,
+        productName: "Airpods 1st Gen",
+        price: "R2000",
+        year: 2016,
+        image: "https://i.postimg.cc/YqRv9c9j/1st-gen.jpg"
+    },
+    {
+        id: 11,
+        productName: "Airpods 2nd Gen",
+        price: "R3000",
+        year: 2019,
+        image: "https://i.postimg.cc/bY3PSqxX/2nd-gen.webp"
+    },
+    {
+        id: 12,
+        productName: "Airpods 3rd Gen",
+        price: "R4000",
+        year: 2021,
+        image: "https://i.postimg.cc/7LMdvNXv/3rd-gen.webp"
+    },
+];
+function displayProducts() {
+    let card = document.querySelector('#card-row');
+    Object.keys(products).forEach( (item)=> {
+        if(products.length){
+            console.log((`${item}: ${products[item]}`));
+            card.innerHTML +=
+            `
+            
+            <div class="card col-4">
+            <img src="${products[item].image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h4 class="></h4>
+            <h5 class="card-title">${products[item].productName}</h5>
+            <p class="card-text">${products[item].price}</p>
+            <a href="#" class="btn btn-primary">Add to checkout</a>
+            </div>
+            </div>
+    
+            `
+        }
+    })
+}
+displayProducts()
+
+localStorage.setItem('products', JSON.stringify(products));
--- js/code.js
+++ js/code.js
@@ -1,181 +1,73 @@
-// let products = JSON.parse(localStorage.getItem('products')) ?
-// JSON.parse(localStorage.getItem('products')) : [
-//     {
-//     id: 1,
-//     Image: "../assets/phonesss.jpg",
-//     productName: "iphone 14",
-//     price : R30000
-//     },
-//     {
-//     id: 2,
-//     productName: "iphone 13",
-//     price : R20000
-//     },
-//     {
-//     id: 3,
-//     productName: "iphone 12",
-//     price : R15000
-//     },
-//     {
-//     id: 4,
-//     Image: "../assets/macbook.jpg",
-//     productName: "macbook pro 16",
-//     price : R50000
-//     },
-//     {
-//     id: 5,
-//     productName: "macbook air",
-//     price : R25000
-//     },
-//     {
-//     id: 6,
-//     productName: "macbook pro 13",
-//     price : R20000
-//     },
-//     {
-//     id: 7,
-//     Image: "../assets/watch1.png",
-//     productName: "smartwatch series 4",
-//     price : R8000
-//     },
-//     {
-//     id: 8,
-//     productName: "smartwatch series 5",
-//     price : R7000
-//     },
-//     {
-//     id: 9,
-//     productName: "smartwatch series 6",
-//     price : R5000
-//     },
-//     {
-//     id: 10,
-//     Image : "../assets/airpods.webp",
-//     productName: "airpods 1st gen",
-//     price : R5000
-//     },
-//     {
-//     id: 11,
-//     productName: "airpods 2nd gen",
-//     price : R3000
-//     },
-//     {
-//     id: 12,
-//     productName: "airpods 3rd gen",
-//     price : R2000
-//     },  
-// ];
-
-//  function displayData() {
-//     products.forEach((products)=> {
-//     document.querySelector('#products').innerHTML += `
-//     <div class="card m-4" style="width: 18rem;">
-//     <img src="${products.Image}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//     <h5 class="card-title">${products.productName}</h5>
-//     <p class="card-text"> Name : ${products.productName}</p>
-//     <p class="card-text"> Price : ${products.price}</p>
-//     <a href="#" class="btn btn-primary">Add to checkout</a>
-//   </div>
-//    </div>`
-//      })
-// };
-// displayData()
-
-
-// Admin Page
-
-let products = JSON.parse(localStorage.getItem('products ')) ?
-JSON.parse(localStorage.getItem('products ')) : [
-    {
-        id: 1,
-        productName: "iPhone 14",
-        price: 30000,
-        year: 2022
-    },
-    {
-        id: 2,
-        productName: "iPhone 13",
-        price: 20000,
-        year: 2021
-    },
-    {
-        id: 3,
-        productName: "Iphone 12",
-        price: 15000,
-        year: 2019
-    },
-    {
-        id: 4,
-        productName: "Macbook Pro 16",
-        price: 50000,
-        year: 2021
-    },
-    {
-        id: 5,
-        productName: "Macbook Air",
-        price: 25000,
-        year: 2019
-    },
-    {
-        id: 6,
-        productName: "Macbook Pro 13",
-        price: 20000,
-        year: 2016
-    },
-    {
-        id: 7,
-        productName: "Apple Smartwatch series 4",
-        price: 8000,
-        year: 2016
-    },
-    {
-        id: 8,
-        productName: "Apple Smartwatch series 5",
-        price: 9000,
-        year: 2019
-    },
-    {
-        id: 9,
-        productName: "Apple Smartwatch series 6",
-        price: 10000,
-        year: 2021
-    },
-    {
-        id: 10,
-        productName: "Airpods 1st Gen",
-        price: 2000,
-        year: 2016
-    },
-    {
-        id: 11,
-        productName: "Airpods 2nd Gen",
-        price: 3000,
-        year: 2019
-    },
-    {
-        id: 12,
-        productName: "Airpods 3rd Gen",
-        price: 4000,
-        year: 2021
-    },
-];
-
-function displayProducts() {
-    let tbody = document.querySelector('tbody');
-    Object.keys(products).forEach( (item)=> {
-        if(products.length){
-            console.log((`${item}: ${products[item]}`));
-            tbody.innerHTML += 
-            `
-            <tr>
-               <td>${products[item].id}</td>
-               <td>${products[item].productName}</td>
-               <td>${products[item].year}</td>
-               <td>${products[item].price}</td>
-            </tr>   
-            `
-        }
-    })
+var selectedRow = null
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
 }
-displayProducts()
+function readFormData() {
+    var formData = {};
+    formData["id"] = document.getElementById("id").value;
+    formData["pName"] = document.getElementById("pName").value;
+    formData["pPrice"] = document.getElementById("pPrice").value;
+    formData["year"] = document.getElementById("year").value;
+    return formData;
+}
+function insertNewRecord(data) {
+    var table = document.getElementById("productList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.id;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.pName;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.pPrice;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.year;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+function resetForm() {
+    document.getElementById("id").value = "";
+    document.getElementById("pName").value = "";
+    document.getElementById("pPrice").value = "";
+    document.getElementById("year").value = "";
+    selectedRow = null;
+}
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("pName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("pPrice").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.id;
+    selectedRow.cells[1].innerHTML = formData.pName;
+    selectedRow.cells[2].innerHTML = formData.pPrice;
+    selectedRow.cells[3].innerHTML = formData.city;
+}
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("productList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("id").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
