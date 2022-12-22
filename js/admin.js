6598586d2804adb5714ac13caaf5ae5589c038a1
Meezaan <meezaandavids365@gmail.com>
2022-12-22 12:05:56 +0200

    update

--- js/admin.js
+++ js/admin.js
@@ -128,18 +128,18 @@ function sort() {
 
     `
     
-//     <div class="dropdown">
-//   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-//     Dropdown button
-//   </button>
-//   <ul class="dropdown-menu">
-//     <li><a class="dropdown-item" href="#">Action</a></li>
-//     <li><a class="dropdown-item" href="#">Another action</a></li>
-//     <li><a class="dropdown-item" href="#">Something else here</a></li>
-//   </ul>
-// </div>
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Sort
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Highest to lowest </a></li>
+    <li><a class="dropdown-item" href="#">Lowest to highest</a></li>
+  </ul>
+</div>
     `
 }
+sort()
 
 function table() {
     let table = 
